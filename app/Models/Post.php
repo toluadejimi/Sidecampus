6be3a17d5d4999f1c6cc6fdbfe2509d9074d6fe7
Jimmy <toluadejimi@gmail.com>
2023-12-30 08:28:57 +0100

    pushhh

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+
+    protected $casts = [
+
+        'likes' => 'integer',
+        'comment' => 'integer',
+    
+
+    ];
 }
