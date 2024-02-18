e7f34d13fd036e2654aadaf4d778a8b708da3533
Jimmy <toluadejimi@gmail.com>
2024-02-18 15:35:40 +0100

    pushh

--- resources/views/admin/book-list.blade.php
+++ resources/views/admin/book-list.blade.php
@@ -64,30 +64,29 @@
                                         <td>
                                             <p class="mb-0">{{ $data->description }}</p>
                                         </td>
-                                        <td><a href="{{ $data->pdf }}"><i
-                                                    class="ri-file-fill text-secondary font-size-18"></i></a></td>
-                                        <td><a href="{{ $data->audio }}"><i
-                                                    class="ri-folder-music-line text-secondary font-size-18"></i></a>
+                                        <td><a href="{{ $data->pdf }}"><i class="ri-file-fill text-secondary font-size-18"></i></a></td>
+                                        <td><a href="{{ $data->audio }}"><i class="ri-folder-music-line text-secondary font-size-18"></i></a>
                                         </td>
                                         <td>{{ $data->reads }}</td>
                                         <td>{{ $data->plays }}</td>
+                                        @if($data->compressed == 1)
+                                        <td>
+                                            <div class="badge badge-pill badge-success">Compressed</div>
+                                        </td>
+                                        @else
+                                        <td>
+                                            <div class="badge badge-pill badge-danger">Uncompressed</div>
+                                        </td>
+                                        
+                                        @endif
                                         <td>
                                             <div class="flex align-items-center list-user-action">
-                                                <a class="bg-primary" data-toggle="tooltip" data-placement="top"
-                                                    title="" data-original-title="Edit"
-                                                    href="edit-book-view?id={{ $data->id }}"><i
-                                                        class="ri-pencil-line"></i></a>
+                                                <a class="bg-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit" href="edit-book-view?id={{ $data->id }}"><i class="ri-pencil-line"></i></a>
 
-                                                 <a class="bg-warning" data-toggle="tooltip" data-placement="top"
-                                                    title="" data-original-title="Compress"
-                                                    href="compress-book?id={{ $data->id }}"><i
-                                                        class="ri-book-line"></i></a>
+                                                <a class="bg-warning" data-toggle="tooltip" data-placement="top" title="" data-original-title="Compress" href="compress-book?id={{ $data->id }}"><i class="ri-book-line"></i></a>
 
-                                                    
-                                                <a class="bg-danger" data-toggle="tooltip" data-placement="top" title=""
-                                                    data-original-title="Delete"
-                                                    href="delete-book?id={{ $data->id }}"><i
-                                                        class="ri-delete-bin-line"></i></a>
+
+                                                <a class="bg-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete" href="delete-book?id={{ $data->id }}"><i class="ri-delete-bin-line"></i></a>
                                             </div>
                                         </td>
 
