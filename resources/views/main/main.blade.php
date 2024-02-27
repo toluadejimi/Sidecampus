804cfc7c7b991f174129880ae59f8785c2a65887
Jimmy <toluadejimi@gmail.com>
2024-02-27 11:19:25 +0100

    inapppurchase added

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -30,6 +30,87 @@
     </div>
     <!-- loader END -->
     <!-- Wrapper Start -->
+    <div class="wrapper">
+
+        @yield('content')
+
+        <!-- Footer -->
+        <footer class="iq-footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item"><a href="privacy-policy.html">Privacy Policy</a></li>
+                            <li class="list-inline-item"><a href="terms-of-service.html">Terms of Use</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6 text-right">
+                        Copyright 2020 <a href="index.html#">Booksto</a> All Rights Reserved.
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer END -->
+        <!-- color-customizer -->
+
+
+
+
+        <!-- color-customizer END -->
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="{{ url('') }}/public/assets/js/jquery.min.js"></script>
+        <script src="{{ url('') }}/public/assets/js/popper.min.js"></script>
+        <script src="{{ url('') }}/public/assets/js/bootstrap.min.js"></script>
+        <!-- Appear JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/jquery.appear.js"></script>
+        <!-- Countdown JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/countdown.min.js"></script>
+        <!-- Counterup JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/waypoints.min.js"></script>
+        <script src="{{ url('') }}/public/assets/js/jquery.counterup.min.js"></script>
+        <!-- Wow JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/wow.min.js"></script>
+        <!-- Apexcharts JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/apexcharts.js"></script>
+        <!-- Slick JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/slick.min.js"></script>
+        <!-- Select2 JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/select2.min.js"></script>
+        <!-- Owl Carousel JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/owl.carousel.min.js"></script>
+        <!-- Magnific Popup JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/jquery.magnific-popup.min.js"></script>
+        <!-- Smooth Scrollbar JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/smooth-scrollbar.js"></script>
+        <!-- lottie JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/lottie.js"></script>
+        <!-- am core JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/core.js"></script>
+        <!-- am charts JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/charts.js"></script>
+        <!-- am animated JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/animated.js"></script>
+        <!-- am kelly JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/kelly.js"></script>
+        <!-- am maps JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/maps.js"></script>
+        <!-- am worldLow JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/worldLow.js"></script>
+        <!-- Raphael-min JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/raphael-min.js"></script>
+        <!-- Morris JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/morris.js"></script>
+        <!-- Morris min JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/morris.min.js"></script>
+        <!-- Flatpicker Js -->
+        <script src="{{ url('') }}/public/assets/js/flatpickr.js"></script>
+        <!-- Style Customizer -->
+        <script src="{{ url('') }}/public/assets/js/style-customizer.js"></script>
+        <!-- Chart Custom JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/chart-custom.js"></script>
+        <!-- Custom JavaScript -->
+        <script src="{{ url('') }}/public/assets/js/custom.js"></script>
 </body>
 
 </html>
