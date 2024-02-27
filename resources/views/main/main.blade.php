fbc0e7f83276a221184338514150dbc65ea1b22b
Jimmy <toluadejimi@gmail.com>
2024-02-27 11:49:43 +0100

    inapppurchase added

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -30,10 +30,131 @@
 </div>
 <!-- loader END -->
 <!-- Wrapper Start -->
+<div class="">
     <!-- TOP Nav Bar -->
     <div class="iq-top-navbar">
         <div class="iq-navbar-custom">
+            <nav class="navbar navbar-expand-lg navbar-light p-0">
+                <div class="iq-menu-bt d-flex align-items-center">
+                    <div class="wrapper-menu">
+                        <div class="main-circle"><i class="las la-bars"></i></div>
+                    </div>
+                    <div class="iq-navbar-logo d-flex justify-content-between">
+                        <a href="index.html" class="header-logo">
+                            <img src="images/logo.png" class="img-fluid rounded-normal" alt="">
+                            <div class="logo-title">
+                                <span class="text-primary text-uppercase">Booksto</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="navbar-breadcrumb">
+                    <h5 class="mb-0">Welcome</h5>
+                    <nav aria-label="breadcrumb">
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="/">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Home Page</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="iq-search-bar">
+                    {{-- <form action="index.html#" class="searchbox">
+                        <input type="text" class="text search-input" placeholder="Search Here...">
+                        <a class="search-link" href="index.html#"><i class="ri-search-line"></i></a>
+                    </form> --}}
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-label="Toggle navigation">
+                    <i class="ri-menu-3-line"></i>
+                </button>
 
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto navbar-list">
+                        {{-- <li class="nav-item nav-icon search-content">
+                            <a href="index.html#" class="search-toggle iq-waves-effect text-gray rounded">
+                                <i class="ri-search-line"></i>
+                            </a>
+                            <form action="index.html#" class="search-box p-0">
+                                <input type="text" class="text search-input" placeholder="Type here to search...">
+                                <a class="search-link" href="index.html#"><i class="ri-search-line"></i></a>
+                            </form>
+                        </li> --}}
+
+
+                        @auth
+                            <li class="line-height pt-3">
+                                <a href="index.html#" class="search-toggle iq-waves-effect d-flex align-items-center">
+                                    <img src="{{ url('') }}/public/assets/images/user/1.jpg"
+                                         class="img-fluid rounded-circle mr-3" alt="user">
+                                    <div class="caption">
+                                        <h6 class="mb-1 line-height">{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</h6>
+                                        <p class="mb-0 text-primary">Click for more</p>
+                                    </div>
+                                </a>
+                                <div class="iq-sub-dropdown iq-user-dropdown">
+                                    <div class="iq-card shadow-none m-0">
+                                        <div class="iq-card-body p-0 ">
+                                            <div class="bg-primary p-3">
+                                                <h5 class="mb-0 text-white line-height">{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</h5>
+                                                @if (Auth::user()->status == 1)
+
+                                                    <span class="text-white font-size-12">Active</span>
+
+                                                @else
+
+                                                    <span class="text-white font-size-12">Inactive</span>
+
+                                                @endif
+
+                                            </div>
+                                            <a href="profile" class="iq-sub-card iq-bg-primary-hover">
+                                                <div class="media align-items-center">
+                                                    <div class="rounded iq-card-icon iq-bg-primary">
+                                                        <i class="ri-file-user-line"></i>
+                                                    </div>
+                                                    <div class="media-body ml-3">
+                                                        <h6 class="mb-0 ">My Profile</h6>
+                                                        <p class="mb-0 font-size-12">View personal profile details.</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="plan" class="iq-sub-card iq-bg-primary-hover">
+                                                <div class="media align-items-center">
+                                                    <div class="rounded iq-card-icon iq-bg-primary">
+                                                        <i class="ri-profile-line"></i>
+                                                    </div>
+                                                    <div class="media-body ml-3">
+                                                        <h6 class="mb-0 ">My Plan</h6>
+                                                        <p class="mb-0 font-size-12">Manage your Subscription</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+
+
+                                            <div class="d-inline-block w-100 text-center p-3">
+                                                <a class="bg-primary iq-sign-btn" href="log-out" role="button">Sign
+                                                    out<i class="ri-login-box-line ml-2"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        @else
+
+                            <div class="d-inline-block w-100 text-center p-3">
+                                <a href="login" class="btn btn-success my-3">Login</a>
+
+                                <a href="register" class="btn btn-danger my-3">Register</a>
+                            </div>
+
+                        @endauth
+
+
+                    </ul>
+                </div>
+            </nav>
         </div>
     </div>
     <!-- TOP Nav Bar END -->
